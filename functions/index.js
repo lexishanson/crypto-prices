ce733b7fb4a3f514759560b37bad473c58b589e4
lexishanson <lexishanson@gmail.com>
2017-07-07 13:19:14 -0700

    formatting changes

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 process.env.DEBUG = 'actions-on-google:*';
@@ -19,56 +18,72 @@ const GET_PRICE_CONTEXT = 'get-price';
 
 // Context Parameters
 const CRYPTO_COIN = 'crypto-coin';
-// const CRYPTO_COIN2 = 'crypto-coin2'; - may need to add back in to solve for crypto to crypto conversion
+const CRYPTO_COIN2 = 'crypto-coin2';
 const CURRENCY_NAME = 'currency-name';
 
 const coinFullNames = {
-  ETH: 'Ethereum',
+  // cryptocurrencies
   BTC: 'Bitcoin',
+  DASH: 'Digital Cash',
+  DOGE: 'Dogecoin',
+  ETC: 'Ethereum Classic',
+  ETH: 'Ethereum',
+  GNT: 'Golem',
+  IOT: 'Iota',
+  LTC: 'Litecoin',
   SC: 'Siacoin',
+  STRAT: 'Stratis',
+  XMR: 'Monero',
+  XRP: 'Ripple',
+  // traditional currencies
+  AUD: 'Australian Dollars',
+  CAD: 'Canadian Dollars',
+  CNY: 'Yuan',
   EUR: 'Euros',
-  USD: 'US Dollars',
+  GBP: 'British Pounds',
+  INR: 'Indian Rupees',
+  JPY: 'Yen',
+  USD: 'US Dollars'
 };
 
 exports.cryptoCurrencyPrices = functions.https.onRequest((request, response) => {
-   console.log('headers: ' + JSON.stringify(request.headers));
-   console.log('body: ' + JSON.stringify(request.body));
+  console.log('headers: ' + JSON.stringify(request.headers));
+  console.log('body: ' + JSON.stringify(request.body));
 
-   const assistant = new Assistant({request: request, response: response});
+  const assistant = new Assistant({request: request, response: response});
 
-   let actionMap = new Map();
-   actionMap.set(GET_PRICE_INTENT, getPrice);
-   assistant.handleRequest(actionMap);
+  let actionMap = new Map();
+  actionMap.set(GET_PRICE_INTENT, getPrice);
+  assistant.handleRequest(actionMap);
 
-   function fetchJSON(url) {
-     return fetch(url)
-       .then(res => res.json())
-       .then(body => {
-         console.log('JSON fetch return');
-         if (body.Response === 'Error') throw body.Message;
-         return body;
-       });
-   }
+  function fetchJSON(url) {
+    return fetch(url).then(res => res.json()).then(body => {
+      if (body.Response === 'Error') {
+        throw body.Message;
+      }
+      return body;
+    });
+  }
 
-   function price(fromCoin, toCoin, options) {
-     options = options || {};
-     const baseUrl = 'https://min-api.cryptocompare.com/data/';
-     let url = `${baseUrl}price?fsym=${fromCoin}&tsyms=${toCoin}`;
+  function price(fromCoin, toCoin, options) {
+    options = options || {};
+    const baseUrl = 'https://min-api.cryptocompare.com/data/';
+    let url = `${baseUrl}price?fsym=${fromCoin}&tsyms=${toCoin}`;
     //  if (options.exchanges) url += `&e=${options.exchanges}`;
     //  if (options.tryConversion === false) url += '&tryConversion=false';
-     return fetchJSON(url);
-   }
+    return fetchJSON(url);
+  }
 
-   function getPrice(assistant) {
-     let fromCoin = assistant.getArgument(CRYPTO_COIN);
-     let toCoin = assistant.getArgument(CURRENCY_NAME) /* || assistant.getArgument(CRYPTO_COIN2) */ || 'USD';
+  function getPrice(assistant) {
+    let fromCoin = assistant.getArgument(CRYPTO_COIN);
+    let toCoin = assistant.getArgument(CURRENCY_NAME) ||
+    assistant.getArgument(CRYPTO_COIN2) ||
+    'USD';
 
-     let getPrice = price(fromCoin, toCoin)
-     .then((body) => body[toCoin])
-     .then(toCoinPrice => {
-       return `The current price of ${coinFullNames[fromCoin]} is $${toCoinPrice} ${coinFullNames[toCoin]}`
-     })
-     .then(message => assistant.tell(message))
-     .catch(console.error);
-   }
+    let getPrice = price(fromCoin, toCoin)
+    .then((body) => body[toCoin])
+    .then(toCoinPrice => {
+      return `The current price of ${coinFullNames[fromCoin]} is $${toCoinPrice} ${coinFullNames[toCoin]}`;})
+    .then(message => assistant.tell(message)).catch(console.error);
+  }
 });
