2748bbb9c1f40361349cc3522b5f9c5175e392ce
Lexis Hanson <lhanson@lhanson-ltm1.internal.salesforce.com>
2017-08-19 15:13:35 -0700

    WIP w/ formatting added

--- functions/index.js
+++ functions/index.js
@@ -25,6 +25,7 @@ const CURRENCY_NAME = 'currency-name';
 const coinFullNames = {
   // cryptocurrencies
   BTC: 'Bitcoin',
+  BCH: 'Bitcoin Cash',
   DASH: 'Digital Cash',
   DOGE: 'Dogecoin',
   ETC: 'Ethereum Classic',
@@ -44,57 +45,88 @@ const coinFullNames = {
   GBP: 'British Pounds',
   INR: 'Indian Rupees',
   JPY: 'Yen',
-  USD: 'US Dollars'
+  USD: 'Dollars',
 };
 
-exports.cryptoCurrencyPrices = functions.https.onRequest((request, response) => {
+const cryptoPrices = (request, response) => {
 
-  const fetchJSON = (url) => {
-    return fetch(url).then(res => res.json()).then(body => {
-      if (body.Response === 'Error') {
-        throw body.Message;
-      }
-      return body;
-    });
+  const getPrice = (assistant) => {
+    let fromCoin = assistant.getArgument(CRYPTO_COIN);
+    let coinNameLongOrShort = assistant.getArgument(CURRENCY_NAME) ||
+    assistant.getArgument(CRYPTO_COIN2) ||
+    'USD';
+
+    let toCoin = shortName(coinNameLongOrShort.toUpperCase());
+    console.log('tocoin from tocoinlf', toCoin);
+
+    if (fromCoin === null) {
+      assistant.ask('Hi, which cryptocoin would you like the price for?');
+    } else {
+      let getPriceRequest =
+      pricePromise(fromCoin, toCoin)
+      .then(getToCoinPriceFromBody(toCoin))
+      // .then(formatToCoinPrice(toCoinPrice))
+      .then(generateMessageFn(fromCoin, toCoin))
+      .then(respondWithMessage)
+      .catch(console.error);
+    }
   };
 
-  const price = (fromCoin, toCoin, options) => {
-    options = options || {};
-    console.log('arguments are: ', arguments);
+  const coins = Object.keys(coinFullNames).map(short => ({
+    shortName: short.toUpperCase(),
+    longName: coinFullNames[short].toUpperCase(),
+  }));
+
+  const shortName = (name) => coins.find((coin) => coin.longName === name || coin.shortName === name).shortName;
+
+  const pricePromise = (fromCoin, toCoin) => {
+    console.log('arguments are: ', fromCoin, toCoin);
     const baseUrl = 'https://min-api.cryptocompare.com/data/';
     let url = `${baseUrl}price?fsym=${fromCoin}&tsyms=${toCoin}`;
     return fetchJSON(url);
   };
 
-  const generateMessageFunction = (coinFullNames, fromCoin, toCoin) => (toCoinPrice) => {
+  const fetchJSON = url => {
+    return fetch(url).then(res => res.json()).then(body => {
+      if (body.Response === 'Error') {
+        throw body.Message;
+      }
+
+      console.log('fetchJSON triggered');
+      return body;
+    });
+  };
+
+  const getToPriceCoinFromBody = toCoin => body => body[toCoin];
+
+  // const formatToCoinPrice = toCoinPrice => {
+  //   if (toCoinPrice && toCoinPrice < 1) {
+  //     return toCoinPrice.toFixed(4);
+  //   }
+  //
+  //   return toCoinPrice.toFixed(2);
+  // };
+
+  const generateMessageFn = (fromCoin, toCoin) => (toCoinPrice) => {
+    if (toCoin === 'USD') {
+      return (`The current price of ${coinFullNames[fromCoin]} is $${toCoinPrice}.`
+       );
+    }
+
     return (`The current price of ${coinFullNames[fromCoin]} is ${toCoinPrice}
-       ${coinFullNames[toCoin]}`
+       ${coinFullNames[toCoin]}.`
      );
   };
 
-  const getPrice = (assistant) => {
-    let fromCoin = assistant.getArgument(CRYPTO_COIN);
-    let toCoin = assistant.getArgument(CURRENCY_NAME) ||
-    assistant.getArgument(CRYPTO_COIN2) ||
-    'USD';
-    if (fromCoin === null) {
-      assistant.ask('Hi, which cryptocoin would you like the price for?');
-    } else {
-      let getPrice = price(fromCoin, toCoin)
-      .then(body => body[toCoin])
-      .then(generateMessageFunction(coinFullNames, fromCoin, toCoin))
-      .then(message => assistant.tell(message))
-      .catch(console.error);
-    }
-  };
+  const respondWithMessage = message => assistant.tell(message);
 
-  console.log('headers: ' + JSON.stringify(request.headers));
-
-  const assistant = new Assistant({request: request, response: response});
+  const assistant = new Assistant({ request: request, response: response });
 
   let actionMap = new Map();
   actionMap.set(WELCOME_INTENT, getPrice);
   actionMap.set(GET_PRICE_INTENT, getPrice);
   assistant.handleRequest(actionMap);
 
-});
+};
+
+exports.cryptoCurrencyPrices = functions.https.onRequest(cryptoPrices);
